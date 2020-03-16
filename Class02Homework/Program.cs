d620790908ec00bf0c695b4db5d8b92126ee7c99
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-03-16 23:31:24 +0100

    Add homework 01

---
+++ Class02Homework/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Class02Homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number:");
+            int userInput1 = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Enter the operation:");
+            string operation = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number:");
+            int userInput2 = int.Parse(Console.ReadLine());
+
+            if(operation == "/")
+            {
+                Console.WriteLine($"The result is:  {userInput1 / userInput2}");
+            }
+            else if (operation == "*")
+            {
+                Console.WriteLine($"The result is:  {userInput1 * userInput2}");
+            }
+            else if (operation == "+")
+            {
+                Console.WriteLine($"The result is:  {userInput1 + userInput2}");
+            }
+            else if (operation == "-")
+            {
+                Console.WriteLine($"The result is:  {userInput1 - userInput2}");
+            }
+            else
+            {
+                Console.WriteLine("Something you enter is wrong!");
+            }
+        }
+    }
+}
