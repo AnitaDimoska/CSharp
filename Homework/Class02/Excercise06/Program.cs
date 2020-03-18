d9a23fb02e417484f6074986213291104493da53
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-03-18 01:18:01 +0100

    Add else at the end

--- Homework/Class02/Excercise06/Program.cs
+++ Homework/Class02/Excercise06/Program.cs
@@ -12,28 +12,32 @@ namespace Excercise06
             Console.WriteLine("Enter second number:");
             double userInput2 = int.Parse(Console.ReadLine());
 
-            if(userInput1 > userInput2)
+            if (userInput1 > userInput2)
             {
                 Console.WriteLine($"Number {userInput1} is larger");
-                if(userInput1 % 2 == 0)
+                if (userInput1 % 2 == 0)
                     Console.WriteLine($"Number {userInput1} is even");
                 else
                     Console.WriteLine($"Number {userInput1} is odd");
             }
-            else if(userInput1 < userInput2)
+            else if (userInput1 < userInput2)
             {
                 Console.WriteLine($"Number {userInput2} is larger");
-                if(userInput2 % 2 == 0)
+                if (userInput2 % 2 == 0)
                     Console.WriteLine($"Number {userInput2} is even");
                 else
                     Console.WriteLine($"Number {userInput2} is odd");
             }
-            else if(userInput1 == userInput2)
+            else if (userInput1 == userInput2)
             {
-                if(userInput1 % 2 == 0)
+                if (userInput1 % 2 == 0)
                     Console.WriteLine("Numbers are equal and even");
                 else
                     Console.WriteLine("Numbers are equal and odd");
+            }
+            else
+            {
+                Console.WriteLine("Something went wrong");
             }
         }
     }
