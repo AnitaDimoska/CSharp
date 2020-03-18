51def1272ff3477aaeec2854f0f3930b5001f2c2
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-03-19 00:54:02 +0100

    Code Excercise07

---
+++ Homework/Class02/Excercise07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Excercise07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vnesi broj od 1 do 3:");
+            int userInput = int.Parse(Console.ReadLine());
+            switch(userInput)
+            {
+                case 1:
+                    Console.WriteLine("You got a new car!");
+                    break;
+                case 2:
+                    Console.WriteLine("You got a new plane!");
+                    break;
+                case 3:
+                    Console.WriteLine("You got a new bike!");
+                    break;
+                default:
+                    Console.WriteLine("Please enter number 1, 2 or 3!");
+                    break;
+            }
+        }
+    }
+}
