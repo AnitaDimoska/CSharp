f8f8092192ab8c8b51ab936bd2905da3e28eda38
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-03-25 23:22:03 +0100

    Homework Class 4 Task 1

---
+++ Homework/Class04/Homework Class 4 Task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_Class_4_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Homework/Class04/Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Substrings()
+            {
+                string mainString = "Hello from SEDC Codecademy v7.0";
+                int stringLength = mainString.Length;
+
+                Console.WriteLine("Please enter number:");
+                int userInput = int.Parse(Console.ReadLine());
+                while (true)
+                {
+                    if (userInput <= stringLength)
+                    {
+                        string newString = mainString.Substring(0, userInput);
+                        Console.WriteLine(newString);
+                        Console.WriteLine(newString.Length);
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Please enter a number smaller than {stringLength}");
+                    }
+                    break;
+                }
+            }
+            Substrings();
+        }
+    }
+}
