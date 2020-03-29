8c314bd422e750d61c14c4445c8ef50d0518c2ea
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-03-29 03:07:45 +0200

    Code task 2 for DateTime

---
+++ Homework/Class04/Task 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime currentDateTime = DateTime.Now;
+            DateTime threeDaysFromNow = currentDateTime.AddDays(3);
+            Console.WriteLine(threeDaysFromNow);
+
+            DateTime oneMonthFromNow = currentDateTime.AddMonths(1);
+            Console.WriteLine(oneMonthFromNow);
+
+            DateTime monthAndThreeDays = oneMonthFromNow.AddDays(3);
+            Console.WriteLine(monthAndThreeDays);
+
+            DateTime oneYearAndTwoMonthsFromNow = currentDateTime.AddYears(-1).AddMonths(-2);
+            Console.WriteLine(oneYearAndTwoMonthsFromNow);
+
+            int month = currentDateTime.Month;
+            Console.WriteLine(month);
+
+            int year = currentDateTime.Year;
+            Console.WriteLine(year);
+        }
+    }
+}
