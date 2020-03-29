2c1f8f638cb155b8b0074f297c0f854864ee4025
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-03-30 01:30:09 +0200

    Code AgeCalculator Code task 1 for Dog class

---
+++ Homework/Class04/Task 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vnesi go datumot na raganje");
+
+            string dateInput = Console.ReadLine();
+            DateTime birthDay = DateTime.Parse(dateInput);
+            DateTime todaysDate = DateTime.Now;
+
+            int yearsOld = todaysDate.Year - birthDay.Year;
+            if (birthDay.Month == todaysDate.Month)
+            {
+                if (birthDay.Day > todaysDate.Day)
+                {
+                    Console.WriteLine($"Imas {yearsOld} godini i {birthDay.Day - todaysDate.Day} den");
+                }
+                else if (birthDay.Day == todaysDate.Day)
+                {
+                    Console.WriteLine($"Imas {yearsOld} godini i sreken rodenden");
+                }
+                else
+                {
+                    Console.WriteLine($"Za {todaysDate.Day - birthDay.Day} dena ke napolnis {yearsOld} godini");
+                }
+            }
+            else if (todaysDate.Month > todaysDate.Month)
+            {
+                Console.WriteLine($"You are {yearsOld} years old i {birthDay.Month - todaysDate.Month} meseci");
+
+            }
+            else
+            {
+                Console.WriteLine($"Ke napolnis {yearsOld} za {birthDay.Month - todaysDate.Month} meseci");
+            }
+        }
+    }
+}
