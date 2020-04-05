85a913492a45c8f5a1b1f4cbe0a4316bffbee292
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-04-05 22:15:13 +0200

    Code Task1 from Class06

---
+++ Homework/Class06/Task1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        public static void NumberStats()
+        {
+            Console.WriteLine("Please enter a number:");
+            string input = Console.ReadLine();
+            double userInput;
+            if (double.TryParse(input, out userInput))
+            {
+                Console.WriteLine($"Stats for number: {userInput}");
+                if (userInput >= 0 && userInput % 2 == 0 && userInput % 1 == 0)
+                    Console.WriteLine("Positive \nInteger \nEven");
+                else if (userInput >= 0 && userInput % 2 != 0 && userInput % 1 == 0)
+                    Console.WriteLine("Positive \nInteger \nOdd");
+                else if (userInput >= 0 &&  userInput % 1 != 0)
+                    Console.WriteLine("Positive \nDecimal");
+                else if (userInput <= 0 && userInput % 1 != 0)
+                    Console.WriteLine("Negative \nDecimal");
+                if (userInput < 0 && userInput % 2 == 0 && userInput % 1 == 0)
+                    Console.WriteLine("Negative \nInteger \nEven");
+                else if (userInput < 0 && userInput % 2 != 0 && userInput % 1 == 0)
+                    Console.WriteLine("Negative \nInteger \nOdd");
+            }
+            else Console.WriteLine("You must enter a number:");
+        }
+
+        public static void CheckAgain()
+        {
+            while (true)
+            {
+                Console.WriteLine("If you want to check number again press Y, if not, press X");
+                string userInput = Console.ReadLine();
+                if (userInput.ToLower() == "x")
+                {
+                    Console.WriteLine("Bye!");
+                    break;
+                }
+                if (userInput.ToLower() == "y")
+                {
+                    NumberStats();
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            NumberStats();
+            CheckAgain();
+        }
+    }
+}
