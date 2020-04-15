0573b326d51e0ec3b5f56efe961e3b708fe9758c
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-04-16 01:37:36 +0200

    Code task03

--- Homework/Class06/Task 3/Program.cs
+++ Homework/Class06/Task 3/Program.cs
@@ -7,103 +7,133 @@ namespace Task_3
         public class Customer
         {
             public string Username { get; set; }
-            public string CardNumber { get; set; }
-            public int Pin { get; set; }
+            public long CardNumber { get; set; }
+            private int Pin { get; set; }
             public double Money { get; set; }
-        }
 
-        public static Customer[] GetCustomers()
-        {
-            var customer1 = new Customer
+            public static Customer[] GetCustomers()
             {
-                Username = "Anna",
-                CardNumber = "123 - 124 - 125",
-                Pin = 1234,
-                Money = 200
-            };
-            var customer2 = new Customer
-            {
-                Username = "Maria",
-                CardNumber = "120 - 124 - 125",
-                Pin = 5678,
-                Money = 2000
-            };
-            var customer3 = new Customer
-            {
-                Username = "Tom",
-                CardNumber = "223 - 224 - 225",
-                Pin = 4321,
-                Money = 5
-            };
-            var customer4 = new Customer
-            {
-                Username = "Bob",
-                CardNumber = "321 - 124 - 125",
-                Pin = 9876,
-                Money = 500
-            };
-            Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
-            return customers;
-
-        }
-        public static void LogIn()
-        {
-            var customers = GetCustomers();
-            Console.WriteLine("Please enter your name:");
-            var username = Console.ReadLine();
-            Console.WriteLine("Please enter a card number");
-            var cardNum = Console.ReadLine();
-
-            foreach (var item in customers)
-            {
-                if (item.Username == username)
+                var customer1 = new Customer
                 {
-                    Console.WriteLine("Please enter your pin");
-                    int pinNum = int.Parse(Console.ReadLine());
-                    if (item.Pin == pinNum)
+                    Username = "Anna",
+                    CardNumber = 1232124312511251,
+                    Pin = 1234,
+                    Money = 200
+                };
+                var customer2 = new Customer
+                {
+                    Username = "Maria",
+                    CardNumber = 1200124012501251,
+                    Pin = 5678,
+                    Money = 2000
+                };
+                var customer3 = new Customer
+                {
+                    Username = "Tom",
+                    CardNumber = 1232120312511251,
+                    Pin = 4321,
+                    Money = 5
+                };
+                var customer4 = new Customer
+                {
+                    Username = "Bob",
+                    CardNumber = 1232124312511251,
+                    Pin = 9876,
+                    Money = 500
+                };
+                Customer[] customers = new Customer[] { customer1, customer2, customer3, customer4 };
+                return customers;
+
+            }
+
+            public void LogIn()
+            {
+
+                var customers = GetCustomers();
+                Console.WriteLine("Please enter your name:");
+                var username = Console.ReadLine();
+                Console.WriteLine("Please enter a card number");
+                var cardNum = Console.ReadLine();
+
+
+                foreach (var item in customers)
+                {
+                    if (item.Username == username && item.CardNumber == MakeCardNumber(cardNum))
                     {
-                        Console.WriteLine($"Welcome {item.Username}! Choose an option:\n 1. Balance checking\n 2. Cash withdrawal\n 3. Cash deposition");
-                        var userInput = Console.ReadLine();
-                        if (userInput == "1")
+                        Console.WriteLine("Please enter your pin");
+                        int pinNum = int.Parse(Console.ReadLine());
+
+                        if (item.GetPin() == pinNum)
                         {
-                            Console.WriteLine($"You have {item.Money}$ on your account.");
-                        }
-                        else if (userInput == "2")
-                        {
-                            CashWithdrawal(item.Money);
-                        }
-                        else if (userInput == "3")
-                        {
-                            CashDeposit(item.Money);
+                            Console.WriteLine($"Welcome {item.Username}!");
+                            while (true)
+                            {
+                                Console.WriteLine($"Choose an option:\n 1. Balance checking\n 2. Cash withdrawal\n 3. Cash deposition\n 4. Exit");
+                                var userInput = Console.ReadLine();
+                                if (userInput == "1")
+                                {
+                                    Console.WriteLine($"You have {item.CheckBalance()}$ on your bank account");
+                                }
+                                else if (userInput == "2")
+                                {
+                                    Console.WriteLine("How much money you want to withdrew?");
+                                    var userwithdraw = int.Parse(Console.ReadLine());
+                                    if (userwithdraw <= item.Money)
+                                        Console.WriteLine($"You withdraw {userwithdraw}$, and now have {item.CashWithdrawal(userwithdraw)}$ left");
+                                    else
+                                    {
+                                        Console.WriteLine("You do not have enough money on your account");
+                                    }
+                                }
+                                else if (userInput == "3")
+                                {
+                                    Console.WriteLine("How much money you want to deposit?");
+                                    var userdeposit = int.Parse(Console.ReadLine());
+                                    Console.WriteLine($"You deposit is {userdeposit}$, and now have {item.CashDeposit(userdeposit)}$");
+                                }
+                                else if (userInput == "4")
+                                {
+                                    Console.WriteLine("Bye!");
+                                    break;
+                                }
+                            }
                         }
                     }
                 }
             }
-        }
-        public static void CashWithdrawal(double money)
-        {
-            Console.WriteLine("How much money you want to withdrew?");
-            var userwithdraw = int.Parse(Console.ReadLine());
-            if (userwithdraw <= money)
+            private double CheckBalance()
             {
-                Console.WriteLine($"You withdraw {userwithdraw}$, and now have {money - userwithdraw}$ left");
+                return Money;
             }
-            else
+            private int GetPin()
             {
-                Console.WriteLine("You do not have enough money on your account");
+                return Pin;
+            }
+
+            public double CashWithdrawal(double money)
+            {
+                return Money -= money;
+            }
+
+            public double CashDeposit(double money)
+            {
+                return Money += money;
+            }
+            public long MakeCardNumber(string cardNum)
+            {
+                var numberCard = string.Format("{0}{1}{2}{3}",
+                    cardNum.Substring(0, 4),
+                    cardNum.Substring(5, 4),
+                    cardNum.Substring(10, 4),
+                    cardNum.Substring(15, 4));
+
+                return long.Parse(numberCard);
             }
         }
-
-        public static void CashDeposit(double money)
-        {
-            Console.WriteLine("How much money you want to pay on your account?");
-            var userDeposit = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Now you have {userDeposit + money}$ on your account");
-        }
-
         static void Main(string[] args)
         {
-            LogIn();
+            var customer = new Customer();
+            customer.LogIn();
         }
     }
 }
