385e300283c6532886fc38f0cd77f64eb8f461f7
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-06-16 22:25:53 +0200

    Updated List<string>

--- Homework/Interfaces/Interfaces/Program.cs
+++ Homework/Interfaces/Interfaces/Program.cs
@@ -9,8 +9,8 @@ namespace Interfaces
         {
             var teacher1 = new Teacher(1, "Ana", "AnaD", "123", "Math");
             var teacher2 = new Teacher(2, "Mia", "MiaS", "123", "English");
-            var student1 = new Student(3, "Marko", "MarkoM", "123", new List<string> { "A, C, E" });
-            var student2 = new Student(4, "Stefan", "Stef", "123", new List<string> { "A, A, B" });
+            var student1 = new Student(3, "Marko", "MarkoM", "123", new List<string> { "A", "C", "E" });
+            var student2 = new Student(4, "Stefan", "Stef", "123", new List<string> { "A", "A", "B" });
             teacher1.PrintUser();
             teacher2.PrintUser();
             student1.PrintUser();
