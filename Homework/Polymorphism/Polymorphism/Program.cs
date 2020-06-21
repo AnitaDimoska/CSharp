ddacafdbd5a20e6363bfa198893550d79abe6cfb
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-06-21 23:39:36 +0200

    Formating the code

--- Homework/Polymorphism/Polymorphism/Program.cs
+++ Homework/Polymorphism/Polymorphism/Program.cs
@@ -10,11 +10,14 @@ namespace Polymorphism
             Dog dog1 = new Dog() { Id = 0, Name = "Riki", Color = "black" };
             Dog dog2 = new Dog() { Id = 2, Name = "Marko", Color = "White" };
             Dog dog3 = new Dog() { Id = 3, Name = "Nola", Color = "Brown" };
+
             Dog.Validate(dog1);
             Dog.Validate(dog2);
             Dog.Validate(dog3);
 
-            List<Dog> dogs = new List<Dog>() { dog1, dog2, dog3};
+            //dog2.Bark();
+
+            List<Dog> dogs = new List<Dog>() { dog1, dog2, dog3 };
             DogShelter.PrintAll(dogs);
 
             Console.ReadLine();
