cf222042318d2879fae975fa4c550c4202c0dc9e
anita.dimoska@yahoo.com <anita.dimoska@yahoo.com>
2020-07-20 21:39:59 +0200

    Code homework for serialization/deserialization

---
+++ Homework/Serializing and Deserializing/Domain/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace Domain
+{
+    public class Dog
+    {
+        public Dog(string name, int age, string color)
+        {
+            Name = name;
+            Age = age;
+            Color = color;
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Color { get; set; }
+    }
+
+}
---
+++ Homework/Serializing and Deserializing/Serializing and Deserializing/Program.cs
@@ -0,0 +1,33 @@
+﻿using Domain;
+using Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serializing_and_Deserializing
+{
+    class Program
+    {
+        private static FileFolderService _fileFolderService { get; set; } = new FileFolderService();
+        public static void PrintDogs(List<Dog> dogs)
+        {
+            foreach (Dog item in dogs)
+            {
+                Console.WriteLine($"{ item.Name} { item.Age } { item.Color}");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter dogs name, age and color:");
+            var name = Console.ReadLine();
+            var age = int.Parse(Console.ReadLine());
+            var color = Console.ReadLine();
+
+            var dog1 = new Dog(name, age, color);
+            _fileFolderService.InsertToJSON(dog1);
+
+            List<Dog> dogs = _fileFolderService.ReadJSON<Dog>().ToList();
+            PrintDogs(dogs);
+        }
+    }
+}
